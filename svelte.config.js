804a01d278468c7d79dcfb9716c94ff8f4354b16
Diego Centella Lopez <diego.centella.60090@usalesiana.edu.bo>
2025-07-03 17:30:22 -0400

    fix: update files

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+        vitePreprocess(),
+        preprocess({
+            scss: {
+                includePaths: ['src/styles']
+            }
+        })
+    ],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
